b73fe411aa106ff6d7a577b0959c280af016a35c
Merouane Atig <merouane.atig@gmail.com>
2015-05-14 16:38:52 +0200

    Refactor duration_policy specs and code

--- backend/level2/lib/duration_policy.rb
+++ backend/level2/lib/duration_policy.rb
@@ -5,22 +5,22 @@ class DurationPolicy
 
     return 0 if duration == 0
 
-    days_for_50_percent = duration - 10
-    if days_for_50_percent > 0
-      total += discount_50_percent(days_for_50_percent)
-      duration -= days_for_50_percent
+    days_above_10 = duration - 10
+    if days_above_10 > 0
+      total += discount_50_percent(days_above_10)
+      duration -= days_above_10
     end
 
-    days_for_30_percent = duration - 4
-    if days_for_30_percent > 0
-      total += discount_30_percent(days_for_30_percent)
-      duration -= days_for_30_percent
+    days_above_4 = duration - 4
+    if days_above_4 > 0
+      total += discount_30_percent(days_above_4)
+      duration -= days_above_4
     end
 
-    days_for_10_percent = duration - 1
-    if days_for_10_percent > 0
-      total += discount_10_percent(days_for_10_percent)
-      duration -= days_for_10_percent
+    days_above_1 = duration - 1
+    if days_above_1 > 0
+      total += discount_10_percent(days_above_1)
+      duration -= days_above_1
     end
 
     total += price_for_one_day
@@ -30,15 +30,15 @@ class DurationPolicy
     @price_per_day
   end
 
-  def discount_10_percent duration
+  def discount_10_percent(duration)
     @price_per_day * duration * 0.9
   end
 
-  def discount_30_percent duration
+  def discount_30_percent(duration)
     @price_per_day * duration * 0.7
   end
 
-  def discount_50_percent duration
+  def discount_50_percent(duration)
     @price_per_day * duration * 0.5
   end
 end

3bb3164dd2898737481b02dcf5c9071d5493fbfd
Merouane Atig <merouane.atig@gmail.com>
2015-06-16 23:43:42 +0200

    Application service processes all rentals

--- backend/level4/lib/application_service.rb
+++ backend/level4/lib/application_service.rb
@@ -1,10 +1,13 @@
 class ApplicationService
-  def initialize(data_reader)
+  def initialize(data_reader, processors)
     @data_reader = data_reader
+    @processors = processors
   end
 
   def calculate_rentals(filename)
-    @data_reader.get_cars(filename)
-    @data_reader.get_rentals(filename)
+    rentals = @data_reader.get_rentals(filename)
+    rentals.each do |rental|
+      rental.process(@processors)
+    end
   end
 end
--- backend/level4/spec/application_service_spec.rb
+++ backend/level4/spec/application_service_spec.rb
@@ -1,25 +1,28 @@
 require 'application_service'
 
 RSpec.describe ApplicationService do
-  it 'reads cars from input' do
+  it 'reads rentals from input' do
     data_reader = double('data_reader')
-    application = ApplicationService.new(data_reader)
+    processors = [double('processor')]
+    application = ApplicationService.new(data_reader, processors)
     filename = 'data.json'
-    allow(data_reader).to receive(:get_rentals)
 
-    expect(data_reader).to receive(:get_cars).with(filename)
+    expect(data_reader).to receive(:get_rentals).with(filename).and_return([])
 
     application.calculate_rentals(filename)
   end
 
-  it 'reads rentals from input' do
+  it 'calls processors on rentals' do
     data_reader = double('data_reader')
-    application = ApplicationService.new(data_reader)
-    filename = 'data.json'
-    allow(data_reader).to receive(:get_cars)
+    processors = [double('price_processor'), double('commission_processor')]
+    application = ApplicationService.new(data_reader, processors)
+    rentals = [double('rental')]
+    allow(data_reader).to receive(:get_rentals).and_return(rentals)
 
-    expect(data_reader).to receive(:get_rentals).with(filename)
+    rentals.each do |rental|
+      expect(rental).to receive(:process).with(processors)
+    end
 
-    application.calculate_rentals(filename)
+    application.calculate_rentals('data.json')
   end
 end
